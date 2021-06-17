16c793b5c13561619510400ea0ebe6e32c8df3c8
FloorBelow <floorbelow@zoho.com>
2021-06-18 05:12:35 +1200

    fixed material export

--- gr2obj/gr2obj.cpp
+++ gr2obj/gr2obj.cpp
@@ -17,11 +17,11 @@ struct MatData {
 };
 
 granny_data_type_definition ImportantMatDataType[] = {
-	{ GrannyStringMember, "diffusePath" },
-	{ GrannyStringMember, "normalPath" },
+	{ GrannyStringMember, "Diffuse" },
+	{ GrannyStringMember, "normal" },
 	{ GrannyStringMember, "detailPath" },
 	{ GrannyStringMember, "diffuse2Path" },
-	{ GrannyStringMember, "specularPath" },
+	{ GrannyStringMember, "Specular" },
 	{ GrannyEndMember}
 };
 
@@ -75,7 +75,9 @@ void TexList(char* file) {
 int main(int argc, char* argv[]) {
 	printf("%d\n", argc);
 	char outBuf[128];
-	for (int arg = 1; arg < argc; arg++) {
+	bool materials = strcmp(argv[1], "-m") == 0;
+	for (int arg = 1 + materials; arg < argc; arg++) {
+
 		std::string outFileName = std::string(argv[arg]);
 		outFileName = outFileName.substr(outFileName.find_last_of('\\') + 1);
 		outFileName = outFileName.replace(outFileName.length() - 3, 3, "obj");
@@ -84,7 +86,14 @@ int main(int argc, char* argv[]) {
 		std::ofstream outFile;
 		outFile.open(outFileName);
 
-		outFileName = outFileName.replace(outFileName.length() - 3, 3, "mtl");
+		std::ofstream matFile;
+		if (materials) {
+			outFileName = outFileName.replace(outFileName.length() - 3, 3, "mtl");
+			matFile.open(outFileName);
+			sprintf(outBuf, "mtllib %s\n", outFileName.c_str());
+			outFile << outBuf;
+		}
+
 
 		granny_file* gr = GrannyReadEntireFile(argv[arg]);
 		granny_file_info* info = GrannyGetFileInfo(gr);
@@ -93,7 +102,8 @@ int main(int argc, char* argv[]) {
 		for (int meshCount = 0; meshCount < info->MeshCount; meshCount++) {
 			granny_mesh* mesh = info->Meshes[meshCount];
 			printf("%s\n", mesh->Name);
-			outFile << "g ";
+			if (materials) outFile << "o ";
+			else outFile << "g ";
 			outFile << mesh->Name;
 			outFile << "\n";
 
@@ -111,13 +121,40 @@ int main(int argc, char* argv[]) {
 			for (int i = 0; i < verts->VertexCount; i++) {
 				GrannyGetSingleVertex(verts, i, verts->VertexType, vert);
 				EsoVert* esoVert = (EsoVert*)vert;
-				sprintf(outBuf, "vt %f %f\n", (float)esoVert->uv[6] / 1024, (float)esoVert->uv[7] / -1024, esoVert->uv[8], esoVert->uv[9]);
+				sprintf(outBuf, "vt %f %f\n", (float)esoVert->uv[6] / 1024, (float)esoVert->uv[7] / -1024);
 				outFile << outBuf;
 			}
 
 			std::cout << "Exporting indices\n";
 			granny_tri_topology* faces = mesh->PrimaryTopology;
 			for (int g = 0; g < faces->GroupCount; g++) {
+
+				if (materials) {
+					granny_material* mat = mesh->MaterialBindings[faces->Groups[g].MaterialIndex].Material;
+					for (int map = 0; map < mat->MapCount; map++) {
+						if (strcmp(mat->Maps[map].Usage, "diffuse") != 0) continue;
+						granny_texture* tex = mat->Maps[map].Material->Texture;
+
+						TextureData texData;
+						GrannyConvertSingleObject(tex->ExtendedData.Type, tex->ExtendedData.Object, ImportantTexDataType, &texData, nullptr);
+						MatData matData;
+						GrannyConvertSingleObject(mat->ExtendedData.Type, mat->ExtendedData.Object, ImportantMatDataType, &matData, nullptr);
+
+						printf("%s %d\n", matData.diffusePath, texData.fileIndex);
+
+						std::string matName = std::string(matData.diffusePath);
+						matName = matName.substr(matName.find_last_of('\\') + 1);
+						matName = matName.substr(0, matName.length() - 4);
+
+						sprintf(outBuf, "usemtl %s\n", matName.c_str());
+						outFile << outBuf;
+						sprintf(outBuf, "newmtl %s\nmap_Kd %d.dds\nKs 0.5 0.5 0.5\nNs 225.0\n", matName.c_str(), texData.fileIndex);
+						matFile << outBuf;
+					}
+					sprintf(outBuf, "g %d\n", faces->Groups[g].MaterialIndex);
+					outFile << outBuf;
+				}
+
 				for (int i = faces->Groups[g].TriFirst * 3 + 2; i < (faces->Groups[g].TriFirst + faces->Groups[g].TriCount) * 3; i += 3) {
 					if (faces->IndexCount > 0) sprintf(outBuf, "f %d/%d %d/%d %d/%d\n", faces->Indices[i - 2] + vertOffset, faces->Indices[i - 2] + vertOffset, faces->Indices[i - 1] + vertOffset, faces->Indices[i - 1] + vertOffset, faces->Indices[i] + vertOffset, faces->Indices[i] + vertOffset);
 					else sprintf(outBuf, "f %d/%d %d/%d %d/%d\n", faces->Indices16[i - 2] + vertOffset, faces->Indices16[i - 2] + vertOffset, faces->Indices16[i - 1] + vertOffset, faces->Indices16[i - 1] + vertOffset, faces->Indices16[i] + vertOffset, faces->Indices16[i] + vertOffset);
@@ -133,100 +170,3 @@ int main(int argc, char* argv[]) {
 		std::cout << "Done!\n\n";
 	}
 }
-
-/*
-int main(int argc, char* argv[]) {
-
-	TexList(argv[1]);
-	return 0;
-
-	std::string outFileName = std::string(argv[1]);
-	outFileName = outFileName.substr(outFileName.find_last_of('\\') + 1);
-	outFileName = outFileName.replace(outFileName.length() - 3, 3, "obj");
-	printf("%s\n", outFileName.c_str());
-
-	std::ofstream outFile;
-	outFile.open(outFileName);
-
-	outFileName = outFileName.replace(outFileName.length() - 3, 3, "mtl");
-	std::ofstream matFile;
-	matFile.open(outFileName);
-
-	granny_file* gr = GrannyReadEntireFile(argv[1]);
-	granny_file_info* info = GrannyGetFileInfo(gr);
-
-	char outBuf[128];
-	sprintf(outBuf, "mtllib %s\n", outFileName.c_str());
-
-	int vertOffset = 1;
-	for (int meshCount = 0; meshCount < info->MeshCount; meshCount++) {
-		granny_mesh* mesh = info->Meshes[meshCount];
-		printf("%s\n", mesh->Name);
-
-		outFile << "o ";
-		outFile << mesh->Name;
-		outFile << "\n";
-
-		std::cout << "Exporting vertices\n";
-		granny_vertex_data* verts = mesh->PrimaryVertexData;
-		void* vert = malloc(64);
-
-		for (int i = 0; i < verts->VertexCount; i++) {
-			GrannyGetSingleVertex(verts, i, verts->VertexType, vert);
-			EsoVert* esoVert = (EsoVert*)vert;
-			sprintf(outBuf, "v %f %f %f\n", esoVert->pos[0], esoVert->pos[1], esoVert->pos[2]);
-			outFile << outBuf;
-		}
-
-		for (int i = 0; i < verts->VertexCount; i++) {
-			GrannyGetSingleVertex(verts, i, verts->VertexType, vert);
-			EsoVert* esoVert = (EsoVert*)vert;
-			sprintf(outBuf, "vt %f %f\n", (float)esoVert->uv[6] / 1024, (float)esoVert->uv[7] / -1024, esoVert->uv[8], esoVert->uv[9]);
-			outFile << outBuf;
-		}
-
-		std::cout << "Exporting indices\n";
-		granny_tri_topology* faces = mesh->PrimaryTopology;
-		for (int g = 0; g < faces->GroupCount; g++) {
-			
-
-			granny_material* mat = mesh->MaterialBindings[faces->Groups[g].MaterialIndex].Material;
-			for (int map = 0; map < mat->MapCount; map++) {
-				if (strcmp(mat->Maps[map].Usage, "diffuse") != 0) continue;
-				granny_texture* tex = mat->Maps[map].Material->Texture;
-
-				TextureData texData;
-				GrannyConvertSingleObject(tex->ExtendedData.Type, tex->ExtendedData.Object, ImportantTexDataType, &texData, nullptr);
-				MatData matData;
-				GrannyConvertSingleObject(mat->ExtendedData.Type, mat->ExtendedData.Object, ImportantMatDataType, &matData, nullptr);
-
-				printf("%s %d\n", matData.diffusePath, texData.fileIndex);
-
-				std::string matName = std::string(matData.diffusePath);
-				matName = matName.substr(matName.find_last_of('\\') + 1);
-				matName = matName.substr(0, matName.length() - 4);
-
-				sprintf(outBuf, "usemtl %s\n", matName.c_str());
-				outFile << outBuf;
-				sprintf(outBuf, "newmtl %s\nmap_Kd %d.dds\nKs 0.5 0.5 0.5\nNs 225.0\n", matName.c_str(), texData.fileIndex);
-				matFile << outBuf;
-			}
-			
-			sprintf(outBuf, "g %d\n", faces->Groups[g].MaterialIndex);
-			outFile << outBuf;
-			for (int i = faces->Groups[g].TriFirst * 3 + 2; i < (faces->Groups[g].TriFirst + faces->Groups[g].TriCount) * 3; i += 3) {
-				if(faces->IndexCount > 0) sprintf(outBuf, "f %d/%d %d/%d %d/%d\n", faces->Indices[i - 2] + vertOffset, faces->Indices[i - 2] + vertOffset, faces->Indices[i - 1] + vertOffset, faces->Indices[i - 1] + vertOffset, faces->Indices[i] + vertOffset, faces->Indices[i] + vertOffset);
-				else sprintf(outBuf, "f %d/%d %d/%d %d/%d\n", faces->Indices16[i - 2] + vertOffset, faces->Indices16[i - 2] + vertOffset, faces->Indices16[i - 1] + vertOffset, faces->Indices16[i - 1] + vertOffset, faces->Indices16[i] + vertOffset, faces->Indices16[i] + vertOffset);
-				outFile << outBuf;
-			}
-		}
-
-		vertOffset += verts->VertexCount;
-		
-	}
-	std::cout << "Done!\n";
-	GrannyFreeFile(gr);
-	outFile.close();
-	return 0;
-}
-*/
